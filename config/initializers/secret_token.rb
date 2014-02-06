d4935734f9ac6498e859ccfad089bcf9987d8a5c
Abhishek-kushwaha <akushwaha@q3tech.com>
2014-02-06 10:57:10 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForumMonster::Application.config.secret_token = '6d5948963d85c6f3d27193305332b68070692e98d80a6819ddaff38796c785766cf29c2cca2ce0f3b4925d8b2119dccc601622b40a283bec8e27f567808a62cb'
