4c484f8fc269cacdea73a519ae8e9aa0fdb79251
Abhishek-kushwaha <akushwaha@q3tech.com>
2014-02-07 19:36:48 +0530

    styled signup and login page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,17 @@
-class CategoriesController < ApplicationController  
-  
-  before_filter :authorize, only:[:new, :edit, :update, :destroy]
+class CategoriesController < ApplicationController
+
+  before_filter :authorize, except:[:index, :show]
   def index
     @categories = Category.all
   end
-  
+
   def new
     @category = Category.new
   end
-  
+
   def create
     @category = Category.new(params[:category])
-    
+
     if @category.save
       flash[:notice] = "Category was successfully created."
       redirect_to forums_url
@@ -19,23 +19,23 @@ class CategoriesController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @category = Category.find(params[:id])
   end
-  
+
   def update
     @category = Category.find(params[:id])
-    
+
     if @category.update_attributes(params[:category])
       flash[:notice] = "Category was updated successfully."
       redirect_to forums_url
     end
   end
-  
+
   def destroy
     @category = Category.find(params[:id])
-    
+
     if @category.destroy
       flash[:notice] = "Category was deleted."
       redirect_to forums_url
